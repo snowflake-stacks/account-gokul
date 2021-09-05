057e34bb693d0401e0da1707d09c8a818d728283
GokulMurugan <gokul29@outlook.com>
2021-09-05 21:05:30 +0530

    Adding resource to Create Test DB

---
+++ main.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_providers {
+    snowflake = {
+      source  = "chanzuckerberg/snowflake"
+      version = "0.25.17"
+    }
+  }
+
+  backend "remote" {
+    organization = "tf-snowflake"
+
+    workspaces {
+      name = "create-test-db"
+    }
+  }
+}
+
+provider "snowflake" {
+  role     = "ACCOUNTADMIN"
+  account  = "fw68141"
+  region   = "eu-west-1"
+  username = "GOKULMURUGAN"
+}
+
+resource "snowflake_database" "demo_db" {
+  name    = "TEST_DB"
+  comment = "Database for Snowflake Terraform demo"
+}
