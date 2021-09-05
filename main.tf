a9cbe6bd28bd6439cbd2dcb7be82aef577adface
GokulMurugan <gokul29@outlook.com>
2021-09-05 22:20:16 +0530

    Updating GitWorkflow to checkout modules

--- main.tf
+++ main.tf
@@ -1,6 +1,12 @@
-module "test_db" {
-  source = "git::git@github.com:snowflake-stacks/module-db.git"
+# module "test_db" {
+#   source = "git::git@github.com:snowflake-stacks/module-db.git"
 
-  database_name    = var.database_name
-  database_comment = var.database_comment
+#   database_name    = var.database_name
+#   database_comment = var.database_comment
+# }
+
+
+resource "snowflake_database" "create_db" {
+  name    = var.database_name
+  comment = var.database_comment
 }
