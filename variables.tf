bba8666fb1f41730ceadce6627ebf90080b976a1
GokulMurugan <gokul29@outlook.com>
2021-09-05 22:23:27 +0530

    Adding variable defaults

--- variables.tf
+++ variables.tf
@@ -1,9 +1,11 @@
 variable "database_name" {
   description = "Name of the Database"
   type        = string
+  default     = "TEST_DB"
 }
 
 variable "database_comment" {
   description = "Description for the Database"
   type        = string
+  default     = "Database for Snowflake Terraform demo"
 }
