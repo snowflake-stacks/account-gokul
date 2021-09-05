f757b6e4446aafc1d6ed87da9d0b8251f77c4ae7
GokulMurugan <gokul29@outlook.com>
2021-09-05 21:50:42 +0530

    Fixing module source

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "test_db" {
-  source = "git@github.com:snowflake-stacks/module-db.git"
+  source = "git::git@github.com:snowflake-stacks/module-db.git"
 
   database_name    = var.database_name
   database_comment = var.database_comment
